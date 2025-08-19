e9d3d916c1aecfd933f3f051cb0081f8a3816af3
Larryz1215 <115794083+Larryz1215@users.noreply.github.com>
2025-08-19 14:32:22 +0800

    testpage-gtm

--- vue-gtm/src/page/test1.vue
+++ vue-gtm/src/page/test1.vue
@@ -5,5 +5,20 @@
     <button id="testBtn" @click="console.log('Click TestBtn')">TestBtn</button>
   </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { onMounted } from 'vue';
+import { useGtm } from '@gtm-support/vue-gtm';
+const gtm = useGtm();
+const logEvent = () => {
+  gtm?.push({
+    event: 'test1_page_view',
+    page: 'test1'
+  });
+ console.log('Test 1 Page Mounted');
+};
+onMounted(() => {
+  logEvent();
+
+});
+</script>
 <style lang="css" scoped></style>
